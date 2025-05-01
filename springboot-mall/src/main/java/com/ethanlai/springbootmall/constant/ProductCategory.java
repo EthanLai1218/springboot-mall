b24e7da9c9adcc36529dc3cc7e237d466a9834ce
EthanLai1218 <ethanlai1218@gmail.com>
2025-05-01 18:00:00 +0800

    商品功能 - 將 category 改成使用 Enum 類型

---
+++ springboot-mall/src/main/java/com/ethanlai/springbootmall/constant/ProductCategory.java
@@ -0,0 +1,9 @@
+package com.ethanlai.springbootmall.constant;
+
+public enum ProductCategory {
+
+    // enum 的值需使用大寫，可使用底線
+    FOOD,
+    CAR,
+    E_BOOK
+}
--- springboot-mall/src/main/java/com/ethanlai/springbootmall/rowmapper/ProductRowMapper.java
+++ springboot-mall/src/main/java/com/ethanlai/springbootmall/rowmapper/ProductRowMapper.java
@@ -1,5 +1,6 @@
 package com.ethanlai.springbootmall.rowmapper;
 
+import com.ethanlai.springbootmall.constant.ProductCategory;
 import com.ethanlai.springbootmall.model.Product;
 import org.springframework.jdbc.core.RowMapper;
 
@@ -14,7 +15,12 @@ public class ProductRowMapper implements RowMapper<Product> {
 
         product.setProductId(resultSet.getInt("product_id"));
         product.setProductName(resultSet.getString("product_name"));
-        product.setCategory(resultSet.getString("category"));
+
+        // String 轉 enum
+        String categoryStr = resultSet.getString("category");
+        ProductCategory category = ProductCategory.valueOf(categoryStr);
+        product.setCategory(category);
+
         product.setImageUrl(resultSet.getString("image_url"));
         product.setPrice(resultSet.getInt("price"));
         product.setStock(resultSet.getInt("stock"));
