990a543b8b5601d57909b921d6d3240197fdbc33
EthanLai1218 <ethanlai1218@gmail.com>
2025-05-01 23:28:18 +0800

    商品功能 - 實作新增商品

--- springboot-mall/src/main/java/com/ethanlai/springbootmall/dao/ProductDao.java
+++ springboot-mall/src/main/java/com/ethanlai/springbootmall/dao/ProductDao.java
@@ -1,8 +1,11 @@
 package com.ethanlai.springbootmall.dao;
 
+import com.ethanlai.springbootmall.dto.ProductRequest;
 import com.ethanlai.springbootmall.model.Product;
 
 public interface ProductDao {
 
     Product getProductById(Integer productId);
+
+    Integer createProduct(ProductRequest productRequest);
 }
--- springboot-mall/src/main/java/com/ethanlai/springbootmall/service/ProductService.java
+++ springboot-mall/src/main/java/com/ethanlai/springbootmall/service/ProductService.java
@@ -1,9 +1,11 @@
 package com.ethanlai.springbootmall.service;
 
+import com.ethanlai.springbootmall.dto.ProductRequest;
 import com.ethanlai.springbootmall.model.Product;
 
 public interface ProductService {
 
     Product getProductById(Integer productId);
 
+    Integer createProduct(ProductRequest productRequest);
 }
