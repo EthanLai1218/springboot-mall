0dc04b26fdaa12b3f3e61a6dd58ddc8b51ff635f
EthanLai1218 <ethanlai1218@gmail.com>
2025-05-07 03:12:51 +0800

    訂單功能 - 查詢訂單列表

--- springboot-mall/src/main/java/com/ethanlai/springbootmall/dao/OrderDao.java
+++ springboot-mall/src/main/java/com/ethanlai/springbootmall/dao/OrderDao.java
@@ -1,11 +1,16 @@
 package com.ethanlai.springbootmall.dao;
 
+import com.ethanlai.springbootmall.dto.OrderQueryParams;
 import com.ethanlai.springbootmall.model.Order;
 import com.ethanlai.springbootmall.model.OrderItem;
 
 import java.util.List;
 
 public interface OrderDao {
+
+    Integer countOrder(OrderQueryParams orderQueryParams);
+
+    List<Order> getOrders(OrderQueryParams orderQueryParams);
 
     Order getOrderById(Integer orderId);
 
--- springboot-mall/src/main/java/com/ethanlai/springbootmall/service/OrderService.java
+++ springboot-mall/src/main/java/com/ethanlai/springbootmall/service/OrderService.java
@@ -1,9 +1,16 @@
 package com.ethanlai.springbootmall.service;
 
 import com.ethanlai.springbootmall.dto.CreateOrderRequest;
+import com.ethanlai.springbootmall.dto.OrderQueryParams;
 import com.ethanlai.springbootmall.model.Order;
 
+import java.util.List;
+
 public interface OrderService {
+
+    Integer countOrder(OrderQueryParams orderQueryParams);
+
+    List<Order> getOrders(OrderQueryParams orderQueryParams);
 
     Order getOrderById(Integer orderId);
 
